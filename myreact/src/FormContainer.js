d6060876763ac30175c942d81f86257335d30fa6
Tigran Keshishyan <tigrankeshishyan1@gmail.com>
2022-11-18 18:18:18 +0100

    Suggest some changes

--- myreact/src/FormContainer.js
+++ myreact/src/FormContainer.js
@@ -1,40 +1,58 @@
-import React, {Component} from "react";
-import FormComponent  from "./FormComponent";
-
+import React, { Component } from 'react';
+import FormComponent from './FormComponent';
 
 class Form extends Component {
-    constructor(){
-      super();
-      this.state ={
-        firstName:"",
-        lastName: "",
-        Age:"",
-        destinations:"",
-        isFriendly:false,
-        gender:"",
-        favColor:"red",
-        };
+  constructor() {
+    super();
 
-      this.handleChange = this.handleChange.bind(this);
-    }
-    
-    handleChange (event){
-      const {name, value, type, checked} = event.target
-       type ==="checkbox" ?
-       this.setState({[name]:checked}) : this.setState ({[name]:value
-        // firstName: event.target.value,
-        // lastName: event.target.value
-          // [event.target.name]:event.target.value
-        });
-    }
-  
-    render(){
-      return(
-      <FormComponent 
-        handleChange={this.handleChange}
-        data={this.state} 
-       />);
-      };
+    this.state = {
+      firstName: '',
+      lastName: '',
+      age: '',
+      destinations: '',
+      isFriendly: false,
+      gender: '',
+      favColor: 'red',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  // handleChange(event) {
+  //   const { name, value, type, checked } = event.target;
+  //   type === 'checkbox' ?
+  //     this.setState({ [name]: checked }) : this.setState({
+  //       [name]: value
+  //       // firstName: event.target.value,
+  //       // lastName: event.target.value
+  //       // [event.target.name]:event.target.value
+  //     });
+  // }
+
+  handleChange = (key, value) => {
+    this.setState({ [key]: value });
+  }
+
+  handleTextChange = (event) => {
+    const { name, value } = event.target;
+    this.handleChange(name, value);
+  }
+
+  handleCheckedChange = (event) => {
+    const { name, checked } = event.target;
+    this.handleChange(name, checked);
+  }
+
+  render() {
+    return (
+      <FormComponent
+        // title="Some title example of parentheses"
+        handleCheckedChange={this.handleCheckedChange}
+        handleChange={this.handleTextChange}
+        data={this.state}
+      />
+    );
+  };
+}
 
 export default Form;
